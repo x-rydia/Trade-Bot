cbc4e8ef1428c7c19da360ac294b5aa954b40ba2
x-rydia <70984848+x-rydia@users.noreply.github.com>
2021-08-03 12:27:57 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,153 +1,179 @@
-import alpaca_trade_api as tradeapi
-import datetime as dt
-import json
+class TenPercentBot:
+    whitelist = []
+    defaultlist = ['AAPL', 'GOOG','AMZN','BA','AA','JPM','JNJ','WMT','V','TSM','TSMC','MA','NVDA','TSLA']
 
-class Algo:
-    def __init__(self):
-        #API information
-        self.api_key = 'PKQW2NLKSTEM08FTL56T'
-        self.api_secret = '03li9945K52bKBZiAZjx7t0DbAloEfp8biFX6iDX'
-        self.base_url = 'https://paper-api.alpaca.makerts'
-        self.api = tradeapi.REST(self.api_key, self.api_secret, self.base_url)
-        
-        #Stonk information
-        self.stonkList = []
-        self.listOfStockData = []
-
-    def check_if_market_open(self):
-        '''Checks if the market is open'''
-        clock = self.api.get_clock()
-        if clock.is_open():
-            return True
-        else: 
-            return False
+    def __init__(self, api_key, api_secret, base_url) -> None:
+        '''Configure API with inputs given at start of object creation'''
+        self.api = tradeapi.REST(api_key, api_secret, base_url)
     
-    #all stock list methods, controls the stocks that can be
-    #interacted with, including getting data, buying, selling, etc.
-    def add_stock_list(self):
-        print("Type 'STOP' to stop, enter only valid Symbols: " )
+    def auto_stocks_whitelist(self) -> None:
+        '''uses csv file of NASDAQ to get tickers 
+        of stocks to automatically whitelist'''
+        nasdaq = pd.read_csv('nasdaq_screener_1627684683783.csv')
+        nasdaqMC, nasdaqTICK = round(nasdaq.iloc[:,5], 2), nasdaq.iloc[:,0]
+        for stockMC in range(len(nasdaqMC)):
+            if nasdaqMC[stockMC] > 100000000000:
+                self.whitelist.append(nasdaqTICK[stockMC])
+        print(self.whitelist)
+
+    def get_stocks_whitelist(self) -> list:
+        '''If 'USE DEFAULT' inputed then default to 
+        well known default list of securities, else
+        use only stocks inputed by user.'''
+        #instructions
+        print('''INPUT VALID TICKERS IN ALL-CAPS, INPUT 'USE DEFAULT' TO USE
+        DEFAULT WHITELIST FOR TRADING. ALGORITHM WILL ONLY ACCOUNT 
+        FOR STOCKS IN WHITELIST. INPUT 'STOP INPUT' TO STOP.''')
         while True:
-            stonk = input('Enter Symbol:')
-            if stonk == 'STOP': 
+            inp = input('Enter Ticker: ')
+            if inp == 'USE DEFAULT':
+                self.whitelist = [item for item in self.defaultlist]
+            elif inp == 'STOP INPUT':
                 break
-            elif stonk in self.stonkList:
-                print('Stock already in list')
-            else:
-                self.stonkList.append(stonk)
-
-    def delete_stock_list(self):
-        print("Type 'STOP' to stop, enter only valid Symbols" )
-        while True:
-            stonk = input('Enter Symbol of Stock to Delete: ')
-            try:
-                self.stonkList.remove(stonk)
-            except ValueError as e:
-                print('Stock not in list')
+            elif inp == 'GO BACK':
+                self.whitelist = self.whitelist[:1]
+                print('value deleted successfully')
+            else: 
+                self.whitelist.append(inp)
+        return self.whitelist
     
-    def auto_stock_list(self, stocks):
-        for s in stocks: self.stonkList.append(s)
+    def collect_data(self) -> list:
+        data = []
+        for stock in self.whitelist:
+            i = self.api.get_barset(stock, '1D')
+            sleep(0.01)
+            i = i.df
+            data.append(i)
+            print(i)
+        return data
     
-    def print_stock_list(self):
-        print(self.stonkList)
+    def identify_buys_in_data_by_ma(self, data: list) -> tuple:
+        '''returns a tuple of lists of tickers
+        that are buys, sells, or holds respectively
+        based on comparison between 10-day and 100-day
+        averages'''
+        buys, sells, holds = [], [], []
 
-    def collect_assets(self):
-        pos = []
-        for stonk in self.stonkList:
-            pos.append(self.api.get_asset(stonk))
-        return pos
-
-#Gets data for each stock in stock list
-    def collect_data(self, start, end):
-        #startTime = str(dt.datetime.now() - dt.timedelta(days=365))
-        try: 
-            startDate = start
-            endDate = end
-            for stock in self.stonkList:
-                response = self.api.get_trades(stock, startDate, endDate, limit=1000)
-                
-                #transcribe API response 
-                fwrite = open(f'responses/{stock}.txt', 'w')
-                fwrite.write(str(response))
-                fwrite.close()
-                
-                data = {}
-                fread = open(f'responses/{stock}.txt', 'r') 
-                
-                #take trade data and turn it into a dictionary
-                    
-                pcount, tcount = 0, 0
-                for line in fread:
-                    if "p" in line: 
-                        data.update({'p'+ str(pcount):line[9:-2]})
-                        pcount += 1
-                    
-                    if "t" in line:
-                        data.update({'t'+ str(tcount):line[9:-3]})
-                        tcount += 1
-                fread.close()
-                self.listOfStockData.append(data)
+        for stock in range(len(data)):
+            #compute 100-day moving average
+            avg100List = round(data[stock].mean(), 2)
+            MA100 = round((1/2)*(avg100List[1] + avg100List[2]), 2)
             
-        except Exception as e:
-            print(e)
-            e, dateTimeOfError = str(e), str(dt.datetime.now())
-            f = open('error_log.txt', 'w')
-            errorEntry = e + 'on ' + dateTimeOfError +  '\n'
-            f.write(errorEntry)
-            f.close()
+            #compute 10-day moving average
+            df10 = data[stock].iloc[90:]
+            avg10List = round(df10.mean(), 2)
+            MA10 = round((1/2)*(avg10List[1] + avg10List[2]), 2)
+            
+            #identify as buy or sell by if up or down
+            #by 10% and append ticker to buy or sell
+            if .90*MA10 > MA100:
+                sells.append(self.whitelist[stock])
+            elif .90*MA100 > MA10:
+                buys.append(self.whitelist[stock])
+            else:
+                holds.append(self.whitelist[stock])
+
+        return (buys, sells, holds)
     
-    def run(self, mode):
-        '''mode 1 is conservative, mode 2 is risky, and mode 3 is 
-        r/wallstreetbets levels of stupid'''
-        
-        #Guarantees the market is open during runtime
-        try: 
-            assert self.check_if_market_open() == True
-        except AssertionError as e:
-            print(e)
-            e, dateTimeOfError = str(e), str(dt.datetime.now())
-            f = open('error_log.txt', 'w')
-            errorEntry ='\n'+ e + 'on ' + dateTimeOfError + '\n' 
-            f.write(errorEntry)
-            raise RuntimeError
+    def buy(self, buys: list) -> None:
+        '''Takes output from identify buys by ma
+        and trades the stocks'''
+        for stock in buys:
+            try:
+                self.api.submit_order(symbol = stock, 
+                                    qty = quantity_constant, 
+                                    side = 'buy',
+                                    type = 'market',
+                                    time_in_force= 'gtc')
+            except exception as e:
+                print(e)
 
-        #risk tolerance modes
-        if mode == 1:
-            while True:
-                #collects data for past year
-                self.collect_data('2020-07-22', '2021-07-22')
-                for stock in self.listOfStockData: 
-                    # for each stock in watchlist:
-                    # filter through dictionary of alternating entries, 
-                    # first representing the price then representing the date
-                    pd_values = stock.values()
-                    for v in pd_values:
-                        if v.index() % 2 == 0:
-                            stock.append([v])
-                        else:
-                            stock[v.index() - 1].append(v)
-                            
+    def sell(self, sells: list) -> None:
+        '''checks to see if any of the stocks identified as 
+        sells are in the portfolio and sells them if they are
+        for more than they were bought for'''
+        for stock in sells:
+            try:
+                self.api.submit_order(symbol = stock, 
+                                    qty = quantity_constant,
+                                    side = 'sell',
+                                    type = 'market',
+                                    time_in_force= 'gtc')
+            except exception as e:
+                print(e)
+
+class FivePercentBot(TenPercentBot):
+    '''Inherits from tenpercent bot and
+    changes the threshold for identifying a 
+    stock as a buy or sell to five percent(from ten)'''
+    def identify_buys_in_data_by_ma(self, data: list) -> tuple:
+        '''returns a tuple of lists of tickers
+        that are buys, sells, or holds respectively
+        based on comparison between 10-day and 100-day
+        averages'''
+        buys, sells, holds = [], [], []
+
+        for stock in range(len(data)):
+            #compute 100-day moving average
+            avg100List = round(data[stock].mean(), 2)
+            MA100 = round((1/2)*(avg100List[1] + avg100List[2]), 2)
+            
+            #compute 10-day moving average
+            df10 = data[stock].iloc[90:]
+            avg10List = round(df10.mean(), 2)
+            MA10 = round((1/2)*(avg10List[1] + avg10List[2]), 2)
+            
+            #identify as buy or sell by if up or down
+            #by 10% and append ticker to buy or sell
+            if .95*MA10 > MA100:
+                sells.append(self.whitelist[stock])
+            elif .95*MA100 > MA10:
+                buys.append(self.whitelist[stock])
+            else:
+                holds.append(self.whitelist[stock])
+
+        return (buys, sells, holds)
+
+class OnePercentBot(TenPercentBot):
+    '''Welp, this shit is gonna be so fucking
+    volatile, it will be a miracle if its profitable.
+    pattern day trader? hell fucking yeah apparently...'''
+    def identify_buys_in_data_by_ma(self, data: list) -> tuple:
+        '''returns a tuple of lists of tickers
+        that are buys, sells, or holds respectively
+        based on comparison between 10-day and 100-day
+        averages'''
+        buys, sells, holds = [], [], []
+
+        for stock in range(len(data)):
+        #compute 100-day moving average
+            avg100List = round(data[stock].mean(), 2)
+            MA100 = round((1/2)*(avg100List[1] + avg100List[2]), 2)
+
+        #compute 10-day moving average
+            df10 = data[stock].iloc[90:]
+            avg10List = round(df10.mean(), 2)
+            MA10 = round((1/2)*(avg10List[1] + avg10List[2]), 2)
+
+        #identify as buy or sell by if up or down
+        #by 10% and append ticker to buy or sell
+            if .99*MA10 > MA100:
+                sells.append(self.whitelist[stock])
+            elif .99*MA100 > MA10:
+                buys.append(self.whitelist[stock])
+            else:
+                holds.append(self.whitelist[stock])
+
+        return (buys, sells, holds)
 
 
 
-                    #CONSERVATIVE STRATEGY HERE
-        
-        if mode == 2:
-            while True:
-                self.collect_data()
-                for stock in self.listOfStockData:
-                    #MODERATE STRATEGY HERE
-        
-        if mode == 3:
-            while True:
-                self.collect_data()
-                for stock in self.listOfStockData:
-                    #BALLS TO THE WALL "STRATEGY" HERE
-
-    
 if __name__ == '__main__':
-    boob = Algo()
-    boob.add_stock_list()
-    boob.collect_data()
-    print(boob.listOfStockData)
+    api_key = 'PKQW2NLKSTEM08FTL56T'
+    api_secret = '03li9945K52bKBZiAZjx7t0DbAloEfp8biFX6iDX'
+    base_url = 'https://paper-api.alpaca.makerts'
+        
+    Main = TenPercentBot(api_key, api_secret, base_url)
+
     
-    
+            
